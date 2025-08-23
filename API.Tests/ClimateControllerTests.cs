9547d25053c4e190642ca18863a085d1d5b8936b
João Lemes <joaolemes17@hotmail.com>
2025-08-23 12:04:09 -0400

    chore > configurando certo os teste agora

---
+++ API.Tests/ClimateControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Threading.Tasks;
+using Xunit;
+
+public class ClimateControllerTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+
+    public ClimateControllerTests(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task History_SemToken_DeveRetornar401()
+    {
+        var response = await _client.GetAsync("/api/climate/history");
+
+        Assert.Equal(System.Net.HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+}
